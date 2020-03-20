b94d778c57a6d255f3f287dc5a4d386f94442c89
patcharapon manuspong <59276850+aomsin1822544@users.noreply.github.com>
2020-03-20 13:49:08 +0700

    Update lntegral l.cpp

--- lntegral l.cpp
+++ lntegral l.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 using namespace std;
 
+
+
+
+
+double fun(double x)
+
+{
+	return x*x;
+}
+
 int main()
 {
+	double a,b,h,sum=0,integral,x;
+	int n;
+	cout << "Enter lnital limit\nn: ";
+	cin >> a;
+	cout << "Enter lnital upper(final)limit\nn: ";
+	cin >> b;
+	cout << "Enter the number of sub\nn:";
+	cin >>n;
+	h=(b-a)/n;
+	int i;
+	for(i=1;i<n;i++)
+	{
+		x=a+i*h;
+		sum=sum+fun(x);
+	}
+	integral = h/2.0*(fun(a)+2*sum+fun(b));
+	cout << "integral is:" << integral ;
+   
+
     
 }
