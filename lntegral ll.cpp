5c467787df7a6db05568b177a4acc0f990574ac2
Lycoriz6112700 <59278010+Lycoriz6112700@users.noreply.github.com>
2020-04-06 15:11:03 +0700

    Update lntegral ll.cpp

--- lntegral ll.cpp
+++ lntegral ll.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
-
-double fun(double x){
-	return (x*x)/2 + 3*x-1/x;
+double fun(double x,double y)
+{
+	return pow(pow(x,4)+pow(y,5),0.5);
 }
 
-double DoubleIntegral(double lowBound1, double lowBound2, int n, double dy)
+double DoubleIntegral(double lowBound1, double lowBound2, int n, double dy, double dx)
 {
 	double Bigsum =0;
+	
 	for(int i = 0; i < n; i++)
 	{
 		double yi = lowBound1+i*dy;
-		double dx = (yi-lowBound2)/n;
 		double Smallsum = 0;
 		
-		for(int j = 0; j < n; j++){
-			double xi = lowBound2+dx*j;
-			double funValue = fun(xi);
+		for(int j = 0; j < n; j++)
+		{
+			double xi = lowBound2+j*dx;
+			double funValue = fun(xi,yi);
 			double rectangleArea = funValue*dx;
-			Smallsum += RectangleArea;
+			Smallsum += rectangleArea;
 		}
  		double secondRectangleArea = Smallsum*dy;
 		Bigsum += secondRectangleArea;
@@ -26,20 +28,27 @@ double DoubleIntegral(double lowBound1, double lowBound2, int n, double dy)
 	return Bigsum;
 }
 
-int main(){
-	double lowBound1, upBound1, lowBound2;
-	int n = 1000000;
-	cout << "lowBound1 = "; 
-	cin >> lowBound1;
-	cout << "upBound1 = ";
-	cin >> upBound1;
-	cout << "lowBound2 = ";
+
+int main()
+{
+	double lowBound1, upBound1, lowBound2, upBound2;
+	int n = 30000;
+	cout << "low x = "; 
 	cin >> lowBound2;
+	cout << "up x = ";
+	cin >> upBound2;
+	cout << "low y = ";
+	cin >> lowBound1;
+	cout << "up y = ";
+	cin >> upBound1; 
+	double dx = (upBound2-lowBound2)/n;
 	double dy = (upBound1-lowBound1)/n;
-	double result = DoubleIntegral(lowBound1, lowBound2, n, dy);
-	cout << result;
+	double result = DoubleIntegral(lowBound1, lowBound2, n, dy, dx);
+	cout << "DoubleIntegral = ";
+	cout << result ;
 	return 0;
 }
+
 // second code//
 #include <iostream>
 #include <cmath>
